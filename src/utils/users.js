f3333243f774158fbad284872597b8fd14da5e74
pachinko <sunpochin@gmail.com>
2020-06-09 00:18:02 +0800

    adding users.

--- src/utils/users.js
+++ src/utils/users.js
@@ -26,8 +26,65 @@ const addUser = ({ id, username, room }) => {
     }
 
     // Store user
-    const user = { id, name, room }
+    const user = { id, username, room }
     users.push(user)
     return user
 }
 
+
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id)
+
+    if (index !== -1) {
+        return users.splice(index, 1)[0]
+    }
+}
+
+console.log(users)
+const userRemoved = removeUser(22)
+
+console.log('userRemoved: ', userRemoved)
+console.log('users: ', users)
+
+
+const getUser = (id) => {
+    return users.find((user) => user.id === id)
+}
+
+const getUsersInRoom = (room) => {
+    room = room.trim().toLowerCase()
+    return users.filter((user) => user.room === room)
+}
+
+addUser({
+    id: 22,
+    username: 'andrew',
+    room: ' south philly'
+})
+
+addUser({
+    id: 42,
+    username: 'mike',
+    room: 'south philly'
+})
+
+addUser({
+    id: 32,
+    username: 'andrew',
+    room: 'center city'
+})
+
+const user = getUser(42)
+console.log(user)
+
+
+const userList = getUsersInRoom('south philly')
+console.log(userList)
+
+
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUserInRoom
+}
